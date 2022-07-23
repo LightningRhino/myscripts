a6210a3e94d6f77dd5c81368b3aeb4f3d0d4e2b3
LightningRhino <49691885+LightningRhino@users.noreply.github.com>
2022-07-23 15:10:45 +0200

    added nfs mount command

--- Linux/vmbackup.sh
+++ Linux/vmbackup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -x
 systemctl start libvirt-nosleep.service
-mkdir /mnt/synology
-mount -t cifs -o credentials=/home/olleg/.smbcredentials //192.168.178.201/sonstiges /mnt/synology/
+mkdir -p /mnt/synology
+#mount -t cifs -o credentials=/home/olleg/.smbcredentials //192.168.178.201/sonstiges /mnt/synology/
+mount -t nfs 192.168.178.201:/volume1/sonstiges /mnt/synology
 #rsync -avhP /var/lib/libvirt/images/win11pro.qcow2 /mnt/synology/kvm/win11pro/
 #rsync -avhP /var/lib/libvirt/qemu/nvram/win11pro_VARS.fd /mnt/synology/kvm/win11pro/
 #virsh dumpxml win11pro > /root/win11pro.xml && mv /root/win11pro.xml /mnt/synology/kvm/win11pro/
